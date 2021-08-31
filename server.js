5c85dbe6ac5e6d02118d1c42a4eb90bf9582550a
Joseph <polanco.joseph1@gmail.com>
2021-08-31 18:08:00 -0400

    final commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const serveStatic = require('serve-static');
+
+const path = require('path');
+
+const app = express();
+
+app.use('/', serveStatic ( path.join(__dirname, '/public' ) ) );
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + 'public/index.html')
+})
+const port = process.env.PORT || 5555;
+
+app.listen(port);
+
+console.log("Now listening on port " + port);
